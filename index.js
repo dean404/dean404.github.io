fe140e0b254d548120a075ea677f9c44186c9240
dean808 <deansingleton1999@gmail.com>
2018-09-04 19:49:18 +0100

    better navbar in mobile view

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let menu = document.getElementById("menu");
 let lastScrollPos = 0;
 let ticking = false;
 let navbar = document.querySelector('nav');
@@ -12,3 +13,25 @@ function stick() {
 		navbar.classList.add("navigationBarBottom");
 	}
 }
+
+// prevent element from being removed beofre animation ends
+function awaitTransition() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 200)
+  });
+}
+
+//show menu
+document.getElementById("first").onclick = function() {
+  menu.style.animation = "fadeIn 200ms ease-in-out";
+  menu.classList.toggle("showHide");
+};
+
+// hide menu
+document.getElementById("cross").onclick = async function() {
+  menu.style.animation = "fadeOut 200ms ease-in-out";
+  let remove = await awaitTransition();
+  menu.classList.toggle("showHide");
+};
