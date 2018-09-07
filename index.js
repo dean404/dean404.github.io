c80e603511659fe3709d17b43bf21df1bb4942ed
dean808 <deansingleton1999@gmail.com>
2018-09-08 00:17:00 +0100

    Fixed bug with navbar on window resize

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 let menu = document.getElementById("menu");
-let burgerMenu = document.getElementById("burger");
-let nav = document.querySelectorAll('nav');
-let navbar = nav[1];
+let burgerIcon = document.getElementById("burger");
+let navbar = document.getElementById("navbar");
+let splashPage = document.getElementById("splash");
 let distanceTop = navbar.offsetTop;
-let ticking = false;
 
+window.onscroll = (() => {
+	stick();
+	slowScroll();
+});
+
+window.onresize = (() => {
+	correctNavbar();
+});
+
+// correct the navbar position when the window size changes
+function correctNavbar() {
+	if (navbar.clientTop == 0 && window.pageYOffset < splash.offsetHeight) {
+		navbar.classList.remove("navigationBarTop");
+		navbar.classList.add("navigationBarBottom");
+	} else {
+		//do nothing
+	}
+	distanceTop = navbar.offsetTop;
+}
+
+// fix navbar to top of page or fix i to the bottom
 function stick() {
-	if (window.pageYOffset >= distanceTop) {
+	correctNavbar();
+	 if (window.pageYOffset >= distanceTop) {
 		navbar.classList.remove("navigationBarBottom");
 		navbar.classList.add("navigationBarTop");
 	} else {
@@ -17,24 +38,35 @@ function stick() {
 
 // prevent element from being removed beofre animation ends
 function awaitTransition() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('resolved');
-    }, 200)
-  });
+	return new Promise(resolve => {
+		setTimeout(() => {
+			resolve("resolved");
+		}, 200)
+	});
 }
 
 //show menu
-document.getElementById("burger").onclick = function() {
-  menu.style.animation = "fadeIn 200ms ease-in-out";
-  menu.classList.toggle("showHide");
-  burgerMenu.style.display = "none";
+document.getElementById("burger").onclick = function () {
+	menu.style.animation = "fadeIn 200ms ease-in-out";
+	menu.classList.toggle("showHide");
+	burgerIcon.style.display = "none";
 };
 
 // hide menu
-document.getElementById("cross").onclick = async function() {
-  menu.style.animation = "fadeOut 200ms ease-in-out";
-  let remove = await awaitTransition();
-  menu.classList.toggle("showHide");
-  burgerMenu.style.display = "block";
+document.getElementById("cross").onclick = async function () {
+	menu.style.animation = "fadeOut 200ms ease-in-out";
+	let remove = await awaitTransition();
+	menu.classList.toggle("showHide");
+	burgerIcon.style.display = "block";
 };
+
+// slow the scroll of te intro text
+function slowScroll() {
+	if (document.body.clientWidth >= 800) {
+		let wScroll = window.pageYOffset;
+		let introBox = document.getElementById("intro");
+		introBox.style.transform = "translate(0px, " + wScroll/2.3 + "%)";	
+	} else {
+		//do nothing
+	}
+}
